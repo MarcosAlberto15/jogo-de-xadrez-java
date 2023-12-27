bef47e12750d6643be7e44fbe5afb1ca711027be
Marcos Alberto <marcos_alberto15@hotmail.com>
2023-12-27 17:03:07 -0300

    moving pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,7 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 import chess.chess.pieces.King;
 import chess.chess.pieces.Rook;
@@ -20,6 +21,28 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validateSoucePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+
+    private Piece makeMove(Position source, Position target) {
+        Piece p = board.removePiece(source);
+        Piece captured = board.removePiece(target);
+        board.placePiece(p, target);
+        return captured;
+    }
+
+
+    private void validateSoucePosition(Position position) {
+        if (!board.thereIsAPiece(position)) {
+            throw new ChessException("Position not on the board!");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
